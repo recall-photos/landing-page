88d7291eeb80336ee5dc2982c164a122c1e5e82a
Jack Veiga <jack@veiga.me>
2018-12-04 15:50:42 +0800

    Add creators name to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,13 +5,21 @@ import styled from 'styled-components'
 
 const Footer = () => (
   <Main>
-    <a href="https://trello.com/b/EMKJ0T7f/recall-roadmap" target="_blank" rel="noopener noreferrer" class="f7 link dim gray dib mr3 mr4-ns helvetica">Roadmap</a>
-    <a href="https://twitter.com/recallphotos" target="_blank" rel="noopener noreferrer" class="f7 link dim gray dib mr3 mr4-ns helvetica">Talk to us</a>
+    <div class="mb3 center">
+        <a href="https://trello.com/b/EMKJ0T7f/recall-roadmap" target="_blank" rel="noopener noreferrer" class="f6 link dim underline black-80 dib mr3 mr4-ns helvetica">Roadmap</a>
+        <a href="https://twitter.com/recallphotos" target="_blank" rel="noopener noreferrer" class="f6 link dim black-80 dib underline mr3 mr4-ns helvetica">Talk to us</a>
+    </div>
+    <div class="center">
+        <p class="f7 gray dib mr2 helvetica">Started by</p>
+        <a href="https://twitter.com/alvesjtiago" target="_blank" rel="noopener noreferrer" class="f7 link dim underline gray dib mr2 helvetica">Tiago Alves</a>
+        <p class="f7 gray dib mr2 helvetica">&</p>
+        <a href="https://twitter.com/recallphotos" target="_blank" rel="noopener noreferrer" class="f7 link dim underline gray dib helvetica">Jack Veiga</a>
+    </div>
   </Main>
 )
 
 const Main = styled.main.attrs({
-  className: 'flex justify-center dt w-100 pb3',
+  className: 'flex flex-column justify-center dt w-100 pb3',
 })``
 
 export default Footer;
